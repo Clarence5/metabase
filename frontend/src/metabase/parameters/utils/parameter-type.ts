9962db8f38750d0f6583f5cbbe2be67f5a648db4
Callum Herries <hi@callumherries.com>
2022-10-18 08:26:37 +0100

    Restrict setting to certain parameter types

--- frontend/src/metabase/parameters/utils/parameter-type.ts
+++ frontend/src/metabase/parameters/utils/parameter-type.ts
@@ -1,7 +1,10 @@
 import _ from "underscore";
 import { FieldFilterUiParameter } from "metabase/parameters/types";
 import { Parameter } from "metabase-types/types/Parameter";
-import { FIELD_FILTER_PARAMETER_TYPES } from "metabase-lib/lib/parameters/constants";
+import {
+  FIELD_FILTER_PARAMETER_TYPES,
+  SINGLE_OR_MULTI_SELECTABLE_TYPES,
+} from "metabase-lib/lib/parameters/constants";
 import { getParameterType } from "metabase-lib/lib/parameters/utils/parameter-type";
 
 export function isFieldFilterParameter(
@@ -9,4 +12,10 @@ export function isFieldFilterParameter(
 ): parameter is FieldFilterUiParameter {
   const type = getParameterType(parameter);
   return FIELD_FILTER_PARAMETER_TYPES.includes(type);
+}
+
+// TODO: maybe move to the dashboard component?
+export function isSingleOrMultiSelectable(parameter: Parameter): boolean {
+  const type = getParameterType(parameter);
+  return SINGLE_OR_MULTI_SELECTABLE_TYPES.includes(type);
 }
